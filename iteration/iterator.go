1b91e02673ee8a14b8aaa58f3d6572852a1274f1
Dale Bandoni <dalebandoni.10@gmail.com>
2024-06-29 14:13:39 +0100

    finished practice exercises for this chapter

--- iteration/iterator.go
+++ iteration/iterator.go
@@ -1,9 +1,16 @@
 package iteration
 
-func Repeat(character string) string {
+import "strings"
+
+func Repeat(character string, repititions int) string {
 	var repeated string
-	for i := 0; i < 5; i++ {
+	for i := 0; i < repititions; i++ {
 		repeated += character
 	}
 	return repeated
+}
+
+func GetIndex(character, substring string) (answer int) {
+	answer = strings.Index(character, substring)
+	return
 }
--- iteration/iterator_test.go
+++ iteration/iterator_test.go
@@ -3,7 +3,7 @@ package iteration
 import "testing"
 
 func TestRepeat(t *testing.T) {
-	got := Repeat("a")
+	got := Repeat("a", 5)
 	want := "aaaaa"
 
 	if got != want {
@@ -13,6 +13,26 @@ func TestRepeat(t *testing.T) {
 
 func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Repeat("a")
+		Repeat("a", 5)
 	}
+}
+
+func TestGetIndex(t *testing.T) {
+	t.Run("Getting first index of 'a' in 'cat'", func(t *testing.T) {
+		got := GetIndex("Cat", "a")
+		want := 1
+
+		if got != want {
+			t.Errorf("got %d want %d", got, want)
+		}
+	})
+
+	t.Run("Getting first index of 'is' in 'tiramisu'", func(t *testing.T) {
+		got := GetIndex("Tiramisu", "is")
+		want := 5
+
+		if got != want {
+			t.Errorf("got %d want %d", got, want)
+		}
+	})
 }
